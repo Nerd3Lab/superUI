22f59dbe000c6feb010ed41ac345fac2dc39d5b6
“Chinapanda” <kittikornchina2014@gmail.com>
2025-02-24 17:15:28 +0700

    feat: Update image and Add notice banner

--- src/renderer/components/layout/MainLayout.tsx
+++ src/renderer/components/layout/MainLayout.tsx
@@ -1,9 +1,9 @@
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
 import styled from 'styled-components';
+import LOGO_IMG from '../../../../assets/img/logo.svg';
 import Carousel from '../utility/Carousel';
 import ChainBall from './ChainBall';
-import LOGO_IMG from '../../../../assets/img/logo.svg';
-import { Icon } from '@iconify/react';
-import { useEffect, useState } from 'react';
 
 interface Props extends SimpleComponent {
   children: React.ReactNode;
@@ -48,8 +48,8 @@ function MainLayout(props: Props) {
 
   return (
     <MainLayoutWrapper className="w-full h-full grid grid-cols-2">
-      <div className="bg-white w-full h-full relative z-100 p-10 flex flex-col justify-center">
-        <div className="bg-white absolute left-6 top-6">
+      <div className="bg-gray-50 w-full h-full relative z-100 p-10 flex flex-col justify-center">
+        <div className="absolute left-6 top-6">
           <img src={LOGO_IMG} alt="logo" className="h-12" />
         </div>
         {/* <div className="bg-white absolute right-6 top-6 flex items-center">
@@ -65,10 +65,10 @@ function MainLayout(props: Props) {
           )}
         </div> */}
         <div className="">{props.children}</div>
-        <p className="bg-white absolute bottom-6 left-6 text-gray-600 text-sm">
+        <p className="absolute bottom-6 left-6 text-gray-600 text-sm">
           © Nerd3Lab 2025
         </p>
-        <p className="flex items-center bg-white absolute bottom-6 right-6 text-gray-600 text-sm">
+        <p className="flex items-center absolute bottom-6 right-6 text-gray-600 text-sm">
           <Icon
             icon="akar-icons:email"
             className="text-brand-500 text-lg mr-2"
@@ -76,26 +76,31 @@ function MainLayout(props: Props) {
           nerd3lab@gmail.com
         </p>
       </div>
-      <div className="w-full flex flex-col justify-end relative items-center">
-        <ChainBall />
-        <div className="w-full px-4 mb-36">
-          <Carousel
-            auto={true}
-            items={[
-              <div className="text-5xl text-black font-semibold text-center">
-                Built on Ethereum,{' '}
-                <span className="text-brand-500">built on the Superchain.</span>
-              </div>,
-              <div className="text-5xl text-black font-semibold text-center">
-                NERD BUILD
-                <span className="text-brand-500"> WEB3</span>
-              </div>,
-              <div className="text-5xl text-black font-semibold text-center">
-                Quickly fire up a personal{' '}
-                <span className="text-brand-500">Superchain blockchain</span>
-              </div>,
-            ]}
-          />
+      <div className="py-4 pr-4 flex bg-gray-50">
+        <div className="w-full flex flex-col justify-end relative items-center bg-white rounded-[20px]">
+          <img src="/img/bg-pattern.png" alt="" className="absolute h-full" />
+          <ChainBall />
+          <div className="w-full px-4 mb-36">
+            <Carousel
+              auto={true}
+              items={[
+                <div className="text-5xl text-black font-semibold text-center">
+                  Built on Ethereum,{' '}
+                  <span className="text-brand-500">
+                    built on the Superchain.
+                  </span>
+                </div>,
+                <div className="text-5xl text-black font-semibold text-center">
+                  NERD BUILD
+                  <span className="text-brand-500"> WEB3</span>
+                </div>,
+                <div className="text-5xl text-black font-semibold text-center">
+                  Quickly fire up a personal{' '}
+                  <span className="text-brand-500">Superchain blockchain</span>
+                </div>,
+              ]}
+            />
+          </div>
         </div>
       </div>
     </MainLayoutWrapper>
--- src/renderer/routes/ProjectRoute.tsx
+++ src/renderer/routes/ProjectRoute.tsx
@@ -2,7 +2,6 @@ import { Icon } from '@iconify/react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Projectitem from '../components/project/Projectitem';
 import ButtonStyled from '../components/utility/ButtonStyled';
 import SerchBox from '../components/utility/SearchBox';
 
@@ -31,6 +30,22 @@ function ProjectRoute(props: Props) {
       <p className="text-gray-600">Recent Projects</p>
       <SerchBox value={search} onChange={handleChange} />
       <div className="flex flex-col gap-2 w-full max-h-[50vh] overflow-scroll">
+        <div className="w-full cursor-pointer rounded-xl flex gap-4 border-1 border-brand-500 bg-brand-50 p-4">
+          <div>
+            <div className="bg-brand-500 p-2.5 rounded-lg shadow-xs">
+              <Icon icon="solar:info-circle-outline" className="text-white" />
+            </div>
+          </div>
+          <div>
+            <div className="text-gray-700 text-md font-semibold">
+              Alpha Version Notice
+            </div>
+            <div className="text-gray-600 text-md">
+              Please note that any data or progress may be lost after closing
+              the session.
+            </div>
+          </div>
+        </div>
         {/* <Projectitem
           name="Nerd3Lab"
           description="deploying blockchain applications."
